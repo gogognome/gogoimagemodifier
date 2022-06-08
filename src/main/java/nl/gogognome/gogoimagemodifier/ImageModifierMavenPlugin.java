6ef36a5c768423ac96992ec75788ac7a9f67a5f0
Guillaume Dufour <guillaume.dufour@arteris.com>
2022-06-08 13:41:03 +0200

    add log and try to remove alpha if save fail

--- src/main/java/nl/gogognome/gogoimagemodifier/ImageModifierMavenPlugin.java
+++ src/main/java/nl/gogognome/gogoimagemodifier/ImageModifierMavenPlugin.java
@@ -8,78 +8,91 @@ import nl.gogognome.gogoimagemodifier.command.AllCommandsExecutor;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.LifecyclePhase;
+import org.apache.maven.plugins.annotations.Mojo;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-/**
- * @goal modifyImage
- * @phase compile
- * @requiresProject false
- */
+@Mojo(name = "modifyImage", defaultPhase = LifecyclePhase.COMPILE, requiresProject = false)
 public class ImageModifierMavenPlugin extends AbstractMojo {
 
-	private final ImageModifier imageModifier = new ImageModifier();
+    private static final Logger LOG = LoggerFactory.getLogger(ImageModifierMavenPlugin.class);
 
-	/**
-	 * The source image file.
-	 * @parameter
-	 */
-	private File sourceImage;
+    private final ImageModifier imageModifier = new ImageModifier();
 
-	/**
-	 * The destination image file.
-	 * @parameter
-	 */
-	private File destinationImage;
+    /**
+     * The source image file.
+     */
+    @Parameter(required = true)
+    private File sourceImage;
 
-	/**
-	 * A list of commands to modify the image.
-	 * @parameter
-	 */
-	private String[] commands;
+    /**
+     * The destination image file.
+     */
+    @Parameter(required = true)
+    private File destinationImage;
 
-	public void execute() throws MojoExecutionException, MojoFailureException {
-		validateParameters();
-		try {
-			imageModifier.loadImage(sourceImage);
-		} catch (IOException e) {
-			throw new MojoFailureException("Failed to load source image: " + e.getMessage());
-		}
+    /**
+     * A list of commands to modify the image.
+     */
+    @Parameter(required = true)
+    private String[] commands;
 
-		for (String command : commands) {
-			executeCommand(command);
-		}
+    public void execute() throws MojoExecutionException, MojoFailureException {
+        validateParameters();
+        try {
+            imageModifier.loadImage(sourceImage);
+            LOG.info("Image loaded " + String.valueOf(sourceImage));
+        } catch (IOException e) {
+            throw new MojoFailureException("Failed to load source image: " + e.getMessage());
+        }
 
-		try {
-			imageModifier.saveImage(destinationImage, getFormatName());
-		} catch (IOException e) {
-			throw new MojoFailureException("Failed to save destination image: " + e.getMessage());
-		}
-	}
+        for (String command : commands) {
+            executeCommand(command);
+        }
 
-	private void executeCommand(String command) throws MojoFailureException {
-		try {
-			boolean executed = new AllCommandsExecutor().execute(imageModifier, command);
-			if (!executed)
-				throw new MojoFailureException("The command \"" + command + "\" could not be executed.");
-		} catch (Exception e) {
-			throw new MojoFailureException("The command \"" + command + "\" could not be executed: " + e.getMessage());
-		}
-	}
+        try {
+            if(!imageModifier.saveImage(destinationImage, getFormatName())) {
+                LOG.warn("Fail to save image, retrying without alpha");
+                imageModifier.removeAlpha();
+                if(!imageModifier.saveImage(destinationImage, getFormatName())) {
+                    throw new MojoFailureException("Failed to save destination image: format is not supported");    
+                }
+            }
+            LOG.info("Image saved " + String.valueOf(destinationImage));
+        } catch (IOException e) {
+            throw new MojoFailureException("Failed to save destination image: " + e.getMessage());
+        }
+    }
 
-	private void validateParameters() throws MojoFailureException {
-		if (sourceImage == null)
-			throw new MojoFailureException("sourceImage was not set.");
+    private void executeCommand(String command) throws MojoFailureException {
+        try {
+            LOG.debug("Executing " + String.valueOf(command));
+            boolean executed = new AllCommandsExecutor().execute(imageModifier, command);
+            LOG.debug("Executed " + String.valueOf(command));
+            if (!executed)
+                throw new MojoFailureException("The command \"" + command + "\" could not be executed.");
+        } catch (Exception e) {
+            throw new MojoFailureException("The command \"" + command + "\" could not be executed: " + e.getMessage());
+        }
+    }
 
-		if (destinationImage == null)
-			throw new MojoFailureException("destinationImage was not set.");
-	}
+    private void validateParameters() throws MojoFailureException {
+        if (sourceImage == null)
+            throw new MojoFailureException("sourceImage was not set.");
 
-	private String getFormatName() throws MojoFailureException {
-		String name = destinationImage.getName();
-		if (name.length() < 3)
-			throw new MojoFailureException("File name of destinationImage is smaller than 3 characters. " +
-					"It must have an extension of .JPG, .GIF or .PNG");
+        if (destinationImage == null)
+            throw new MojoFailureException("destinationImage was not set.");
+    }
 
-		return name.substring(name.length() - 3).toUpperCase();
-	}
+    private String getFormatName() throws MojoFailureException {
+        String name = destinationImage.getName();
+        if (name.length() < 3)
+            throw new MojoFailureException("File name of destinationImage is smaller than 3 characters. " +
+                    "It must have an extension of .JPG, .GIF or .PNG");
+
+        return name.substring(name.length() - 3).toUpperCase();
+    }
 
 }
